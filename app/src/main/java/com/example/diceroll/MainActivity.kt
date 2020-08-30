3d9f4cb8c2785048ce85e0acdda41a2c655a79b0
Venkata Harish Kumar Mamidi <mvhk@pm.me>
2020-08-31 00:13:13 +0530

    comments added

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -3,28 +3,29 @@ package com.example.diceroll
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+/*
+This activity allows you to roll a dice by the user
+*/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var rollButton: Button = findViewById(R.id.rollbutton)
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
-            rollDice()
-        }
+        val rollButton: Button = findViewById(R.id.rollbutton)
+        rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        var textviewNumber: TextView = findViewById(R.id.randomnumberText)
+        val textviewNumber: TextView = findViewById(R.id.randomnumberText)
         textviewNumber.text = diceRoll.toString()
     }
 }
+
 
 class Dice(val numSides: Int) {
 
