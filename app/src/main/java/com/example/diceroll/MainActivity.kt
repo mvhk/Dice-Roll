daeb922c065520c124b8f8ca3ed08b18c87a6f99
Venkata Harish Kumar Mamidi <mvhk@pm.me>
2020-08-31 15:04:06 +0530

    Final Commit

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroll
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /*
@@ -16,20 +16,40 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.rollbutton)
         rollButton.setOnClickListener { rollDice() }
+        // Do a dice roll when the app starts
+        rollDice()
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val textviewNumber: TextView = findViewById(R.id.randomnumberText)
-        textviewNumber.text = diceRoll.toString()
+
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
 
-class Dice(val numSides: Int) {
-
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
+class Dice(private val numSides: Int) {
+    fun roll(): Int = (1..numSides).random()
 }
